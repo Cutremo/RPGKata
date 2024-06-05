6c3885d717445b51de7bc6be729d4756380cea50
Ángel Siendones Sillero <angel100dones@gmail.com>
2024-06-05 10:37:52 +0200

    feat: move prop to file

---
+++ RPGCombatTests/Prop.cs
@@ -0,0 +1,22 @@
+﻿namespace RPGCombatTests;
+
+public class Prop
+{
+
+    Prop(int maxHealth)
+    {
+        if(maxHealth <= 0)
+            throw new ArgumentOutOfRangeException(nameof(maxHealth),
+                "maxHealth must be greater than or equal to zero.");
+        MaxHealth = maxHealth;
+    }
+
+    public static Prop Tree => new(2000);
+    public int MaxHealth { get; private set; }
+    public bool Destroyed => MaxHealth == 0;
+
+    public void TakeDamage(int amount)
+    {
+        MaxHealth = Math.Max(0, MaxHealth - amount);
+    }
+}
--- RPGCombatTests/PropsTests.cs
+++ RPGCombatTests/PropsTests.cs
@@ -55,24 +55,3 @@ public class PropsTests
         doc.Destroyed.Should().BeTrue();
     }
 }
-
-public class Prop
-{
-
-    Prop(int maxHealth)
-    {
-        if(maxHealth <= 0)
-            throw new ArgumentOutOfRangeException(nameof(maxHealth),
-                "maxHealth must be greater than or equal to zero.");
-        MaxHealth = maxHealth;
-    }
-
-    public static Prop Tree => new(2000);
-    public int MaxHealth { get; private set; }
-    public bool Destroyed => MaxHealth == 0;
-
-    public void TakeDamage(int amount)
-    {
-        MaxHealth = Math.Max(0, MaxHealth - amount);
-    }
-}
